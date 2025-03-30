2bdd2d99291d06a8a45a84bf96a22bb993e91c5c
Alex Freidah <alexfreidah@Alexs-MacBook-Pro-2.local>
2025-03-30 12:01:02 -0700

    minor tweaks

---
+++ provision.rb
@@ -0,0 +1,10 @@
+#!/usr/bin/env ruby
+
+# TODO: install chef
+#
+# TODO: run chef-solo or chef-zero to provision this machine
+
+# TODO: run stow to symlink all the dotfiles in this repo
+# we need to leep over every directory in this repo
+# and run `stow <directoryname>` on it to symlink the files
+# to where they belong for confiuration

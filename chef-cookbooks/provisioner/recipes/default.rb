292c159afc339aeecae43f3c1fc6e532b61c70b3
Alex Freidah <alexfreidah@Alexs-MacBook-Pro-2.local>
2025-03-30 20:25:03 -0700

    tweaking terraform stuff

--- chef-cookbooks/provisioner/recipes/default.rb
+++ chef-cookbooks/provisioner/recipes/default.rb
@@ -1,4 +1,4 @@
 include_recipe 'provisioner::packages'
 include_recipe 'provisioner::stow'
 include_recipe 'provisioner::ruby'
-include_recipe 'provisioner::terraform'
+include_recipe 'provisioner::terraform' unless node['platform'] == "mac_os_x"

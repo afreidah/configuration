bc48d2acade1d25b909824722df7a8d98be0aad0
Alex Freidah <alexfreidah@Alexs-MacBook-Pro-2.local>
2025-03-30 20:39:41 -0700

    more terraform tweaks

--- chef-cookbooks/provisioner/attributes/default.rb
+++ chef-cookbooks/provisioner/attributes/default.rb
@@ -1,4 +1,4 @@
-default['provisioner']['packages'] = %w(nvim zsh git tmux stow curl tmuxinator zsh-autosuggestions zsh-syntax-highlighting rbenv unzip)
+default['provisioner']['packages'] = %w(nvim zsh git tmux stow curl tmuxinator zsh-autosuggestions zsh-syntax-highlighting rbenv unzip cowsay)
 default['provisioner']['amd64_packages'] = %w(tfenv tmuxinator-completion)
 default['provisioner']['x86_64_packages'] = %w(build_essential libssl-dev libreadline-dev zlib1g-dev)
 default['provisioner']['ruby_version'] = '3.3.0'
--- chef-cookbooks/provisioner/bootstrap.sh
+++ chef-cookbooks/provisioner/bootstrap.sh
@@ -6,12 +6,14 @@ sudo rm -rf berks-cookbooks local-mode-cache nodes cinc_guid
 # INSTALL CHEF IF NOT INSTALLED
 if ! command -v cinc-solo >/dev/null 2>&1; then
   curl -L https://www.opscode.com/chef/install.sh | sudo bash -s -- -P cinc-workstation -v 24
-end
+fi
 
 # RUN CHEF
 bundle install
+bundle exec berks update
 bundle exec berks install
 bundle exec berks vendor
 sudo cinc-solo -c solo.rb -j solo.json
 
-echo "Bootstrap complete"
+sudo rm -rf berks-cookbooks local-mode-cache nodes cinc_guid
+cowsay "Bootstrap complete!"
